7eaffb1edbf03096fc1d33e9dd1dc54adeb26dc6
Hugo Duncan <hugo@hugoduncan.org>
2015-01-04 21:32:13 -0500

    Update build to remove hooks

--- profiles.clj
+++ profiles.clj
@@ -1,44 +1,58 @@
 {:provided {:dependencies [[org.clojure/clojure "1.6.0"]
                            [org.clojure/clojurescript "0.0-2277"]
                            [prismatic/schema "0.2.6"]
-                           [com.keminglabs/cljx "0.4.0"]]}
+                           [com.keminglabs/cljx "0.5.0"]]}
 
- :dev {:plugins [[lein-pallet-release "RELEASE"]
-                 [com.cemerick/austin "0.1.5"]
-                 [com.cemerick/clojurescript.test "0.3.1"]
-                 [lein-cljsbuild "1.0.3"]
-                 [com.keminglabs/cljx "0.4.0"]]
-       :dependencies [[org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
-       :cljx {:builds [{:source-paths ["test"]
-                        :output-path "target/generated/test/clj"
-                        :rules :clj}
-                       {:source-paths ["test"]
-                        :output-path "target/generated/test/cljs"
-                        :rules :cljs}]}
-       :cljsbuild {:test-commands
-                   {"tests"      ["phantomjs" "runners/runner-none.js"
-                                  "target/unit-test" "target/unit-test.js"]
-                    ;; "node-tests" ["node" "runners/runner-none-node.js"
-                    ;;               "target/unit-test-node"
-                    ;;               "target/unit-test-node.js"]
-                    }
-                   :builds
-                   [{:id "test"
-                      :source-paths ["target/generated/src/clj"
-                                     "target/generated/src/cljs"
-                                     "target/generated/test/cljs"]
-                      :compiler {:output-to "target/unit-test.js"
-                                 :output-dir "target/unit-test"
-                                 :source-map "target/unit-test.js.map"
-                                 :optimizations :none
-                                 :pretty-print true}}
-                    ;; {:id "test-node"
-                    ;;   :source-paths ["target/generated/src/clj"
-                    ;;                  "target/generated/src/cljs"
-                    ;;                  "target/generated/test/cljs"]
-                    ;;   :compiler {:output-to "target/unit-test-node.js"
-                    ;;              :target :nodejs
-                    ;;              :output-dir "target/unit-test-node"
-                    ;;              :optimizations :none
-                    ;;              :pretty-print true}}
-                    ]}}}
+ :cljs-test {:cljx
+             {:builds
+              ;; Using :replace and repeating the src paths, as
+              ;; otherwise test paths get generated multiple times.
+              ^:replace [{:source-paths ["src"]
+                          :output-path "target/generated/src/clj"
+                          :rules :clj}
+                         {:source-paths ["src"]
+                          :output-path "target/generated/src/cljs"
+                          :rules :cljs}
+                         {:source-paths ["test"]
+                          :output-path "target/generated/test/clj"
+                          :rules :clj}
+                         {:source-paths ["test"]
+                          :output-path "target/generated/test/cljs"
+                          :rules :cljs}]}
+             :cljsbuild
+             ^:replace
+             {:test-commands
+              {"tests"      ["phantomjs" "runners/runner-none.js"
+                             "target/unit-test" "target/unit-test.js"]
+               ;; "node-tests" ["node" "runners/runner-none-node.js"
+               ;;               "target/unit-test-node"
+               ;;               "target/unit-test-node.js"]
+               }
+              :builds
+              [{:id "test"
+                :source-paths ["target/generated/src/clj"
+                               "target/generated/src/cljs"
+                               "target/generated/test/cljs"]
+                :compiler {:output-to "target/unit-test.js"
+                           :output-dir "target/unit-test"
+                           :source-map "target/unit-test.js.map"
+                           :optimizations :none
+                           :pretty-print true}}
+               ;; {:id "test-node"
+               ;;   :source-paths ["target/generated/src/clj"
+               ;;                  "target/generated/src/cljs"
+               ;;                  "target/generated/test/cljs"]
+               ;;   :compiler {:output-to "target/unit-test-node.js"
+               ;;              :target :nodejs
+               ;;              :output-dir "target/unit-test-node"
+               ;;              :optimizations :none
+               ;;              :pretty-print true}}
+               ]}
+             }
+ :dev-base {:plugins [[lein-pallet-release "RELEASE"]
+                      [com.cemerick/austin "0.1.5"]
+                      [com.cemerick/clojurescript.test "0.3.3"]
+                      [lein-cljsbuild "1.0.3"]
+                      [com.keminglabs/cljx "0.5.0"]]}
+ :dev [:dev-base :cljs-test]
+ :test [:cljs-test]}
--- project.clj
+++ project.clj
@@ -4,12 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.palletops/api-builder "0.3.0"]]
-  :hooks [cljx.hooks leiningen.cljsbuild]
-  :prep-tasks ["javac" "compile"]
+  :prep-tasks [["cljx" "once"]]
   :source-paths ["target/generated/src/clj"]
   :resource-paths ["target/generated/src/cljs"]
   :test-paths ["target/generated/test/clj"]
-  :aliases {"auto-test" ["do" "clean," "cljsbuild" "auto" "test"]}
+  :aliases {"compile" ["do" "compile," "cljsbuild" "once"]
+            "test" ["do" "test," "cljsbuild" "once" "test," "cljsbuild" "test"]
+            "auto-test" ["do" "clean," "cljsbuild" "auto" "test"]}
   :cljsbuild {:builds []}
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/generated/src/clj"
